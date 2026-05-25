bc080ccd8341ea1e38f1e7db43541e4819efdb0f
Josh Baker <JoshBaker529@Gmail.com>
2026-05-25 12:59:28 -0400

    Added tests for header.get_flag_Playchoice_10

---
+++ tests/src/flag_Playchoice_10.cpp
@@ -0,0 +1,39 @@
+
+
+#include "../../src/header.hpp"
+#include <fstream>
+#include <iostream>
+
+int flag_Playchoice_10(int argc, char *argv[]) {
+  std::ifstream inFile;
+  Header header;
+  bool value;
+
+  inFile.open("../../inputs/flag_Playchoice_10_on.nes_test");
+  if (!inFile) {
+    std::cout << "Could not open on file.";
+    return -1;
+  }
+  header.read(inFile);
+  inFile.close();
+  value = header.get_flag_Playchoice_10();
+  if (!value) {
+    std::cout << "Function returned false, should have been true.";
+    return 1;
+  }
+
+  inFile.open("../../inputs/flag_Playchoice_10_off.nes_test");
+  if (!inFile) {
+    std::cout << "Could not open on file.";
+    return -1;
+  }
+  header.read(inFile);
+  inFile.close();
+  value = header.get_flag_Playchoice_10();
+  if (value) {
+    std::cout << "Function returned true, should have been false.";
+    return 1;
+  }
+
+  return 0;
+}
