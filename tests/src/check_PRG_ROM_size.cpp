e022b7e029f0efd17a51d419fe8a425b803ff23e
Josh Baker <JoshBaker529@Gmail.com>
2026-05-26 13:16:17 -0400

    Added tests for the MSB in byte 8 and for exponent-multiplier formatting

--- tests/src/check_PRG_ROM_size.cpp
+++ tests/src/check_PRG_ROM_size.cpp
@@ -10,6 +10,8 @@ int check_PRG_ROM_size(int argc, char *argv[]) {
 
   std::ifstream inFile;
   Header header;
+
+  // Size of 0
   inFile.open("../../inputs/prg_size_0.nes_test");
   if (!inFile) {
     std::cout << "Could not open file\n";
@@ -24,6 +26,7 @@ int check_PRG_ROM_size(int argc, char *argv[]) {
     return 1;
   }
 
+  // Size of 0FF
   inFile.open("../../inputs/prg_size_255.nes_test");
   if (!inFile) {
     std::cout << "Could not open file\n";
@@ -37,6 +40,32 @@ int check_PRG_ROM_size(int argc, char *argv[]) {
     std::cout << "Incorrect size: " << size << " should be "
               << (0xFF * byte_conversion) << "\n";
     return 2;
+  }
+
+  // Size of EFF
+  inFile.open("../../inputs/prg_size_EFF.nes_test");
+  header.read(inFile);
+  inFile.close();
+  size = header.get_PRG_ROM_size();
+  if (size != (0xEFF * byte_conversion)) {
+    std::cout << "Incorrect size for 0xEFF: " << size << " should be "
+              << (0xEFF * byte_conversion) << "\n";
+    return 3;
+  }
+
+  // Size of exponent-multiplier formatting
+  inFile.open("../../inputs/prg_size_exp_mul_format.nes_test");
+  header.read(inFile);
+  inFile.close();
+  size = header.get_PRG_ROM_size();
+
+  // This file has the LSB as 0x72 and the exponent nibble of F
+  // Should have 2 ^ 28 * 5 bytes, or 1,342,117,280 bytes
+  // I just picked numbers, there is no significance to them
+  if (size != 1'342'177'280) {
+    std::cout << "Incorrect size: " << size << " should be " << 1'342'177'280
+              << "\n";
+    return 4;
   }
 
   return 0;
