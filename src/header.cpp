73bb933b90e0f4004541e711d8d5bfbf8996ece4
Josh Baker <JoshBaker529@Gmail.com>
2026-05-26 13:15:20 -0400

    Fixed get_PRG_ROM_size to account for the MSB in byte 9, and for exponent-multiplier formatting

--- src/header.cpp
+++ src/header.cpp
@@ -1,7 +1,9 @@
 
 #include "header.hpp"
+#include <cmath>
 #include <cstddef>
 #include <cstdint>
+#include <iostream>
 
 static const int kilobyte = 1024;
 
@@ -29,7 +31,47 @@ bool Header::check_nes() {
   return true;
 }
 
-int Header::get_PRG_ROM_size() { return raw_header[4] * (16 * kilobyte); }
+int Header::get_PRG_ROM_size() {
+  int value = 0;
+
+  // MSB
+  int msb = raw_header[9] & 0xF;
+
+  // Is it in exponent-multiplier format?
+  if (msb == 0xF) {
+
+    /*
+      Multiplier formatting:
+
+      EEEE EEMM
+      |||| ||++---- Multipler: MM * 2 + 1
+      ++++-++------ Exponent:  2 ^ EEEEEE
+
+      Final calculation:
+      [ 2 ^ EEEEEE ] * [ MM * 2 + 1 ]
+
+
+    */
+
+    int lsb = raw_header[4];
+    int multiplier = lsb & 3;
+    int exponent = (lsb & ~3) >> 2;
+
+    value = std::pow(2, exponent);
+    value *= (multiplier * 2 + 1);
+
+    return value;
+  }
+
+  // Not exponent-multiplier format
+  value |= (msb << 8);
+  // LSB
+  value |= raw_header[4];
+
+  // value now holds the 12-bit size
+  int size = (16 * kilobyte);
+  return (value * size);
+}
 
 int Header::get_CHR_ROM_size() { return raw_header[5] * (8 * kilobyte); }
 
